0b3626328cc5b5d1a12e12f0211be916d188479a
unknown <nataliematos12@gmail.com>
2023-05-19 15:01:02 -0400

    commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
-mongoose.connect(PORT ? process.env.DB_URL : 'mongodb://127.0.0.1:27017/journey_journal_db');
+mongoose.connect(PORT ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/journey_journal_db');
 
 module.exports = mongoose.connection;
